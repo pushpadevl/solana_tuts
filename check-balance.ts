e41f0a22ad4e9229229f390f56213a7e682b676a
Pushpendra Pal <102184982+pushpadevl@users.noreply.github.com>
2024-06-06 23:51:45 +0530

    Delete check-balance.ts

--- check-balance.ts
+++
@@ -1,8 +0,0 @@
-import { Connection, PublicKey, clusterApiUrl } from "@solana/web3.js";
-
-const conn = new Connection(clusterApiUrl("devnet"));
-const address = new PublicKey('CenYq6bDRB7p73EjsPEpiYN7uveyPUTdXkDkgUduboaN');
-const balance = await conn.getBalance(address);
-
-console.log(`balance at ${address} is ${balance} lamports`);
-console.log(`:;`);
